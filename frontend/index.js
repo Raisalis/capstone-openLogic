6a62d702c7e9a25bea857ca8497dfa8e8cfd9d72
Emma <71865150+Raisalis@users.noreply.github.com>
2022-05-07 18:04:41 -0700

    Publish Assignments Functionality

--- frontend/index.js
+++ frontend/index.js
@@ -452,7 +452,7 @@ function publishAssignments() {
    var checkboxes = document.getElementById('checkboxHolder');
    var className = document.getElementById('classForPublish').value;
    if(checkboxes.innerHTML != "") {
-      var assignments = document.getElementsByTagName('checkOption');
+      var assignments = document.getElementsByName('checkOption');
       console.log(assignments);
       for(var i = 0; i < assignments.length; i++) {
          var assignmentDetails = getAssignmentDetails(className, assignments[i].value);
